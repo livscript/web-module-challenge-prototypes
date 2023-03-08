0b56e3167e2dbaa2129b482f6a1dddc726567de1
Oliwia Kwiatek <livkwiatek@gmail.com>
2023-03-07 22:40:35 -0500

    s2, m3

--- index.js
+++ index.js
@@ -15,11 +15,31 @@
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
+function Person(name, age) {
+  this.name = name;
+  this.age = age
+  this.stomach = []
 
 }
+Person.prototype.eat = function(edible){
+  if(this.stomach.length < 10 ){
+    this.stomach.push(edible)
+  }
 
-
+}
+Person.prototype.poop = function(){
+  this.stomach = []
+}
+Person.prototype.toString = function(){
+  return `${this.name}, ${this.age}`
+}
+const Mary = new Person('Mary', 50)
+console.log(Mary.toString())
+Mary.eat('pizza')
+console.log(Mary.stomach)
+Mary.poop()
+console.log(Mary.stomach)
+// returns marry,50, pizza, []
 /*
   TASK 2
     - Write a Car constructor that initializes `model` and `milesPerGallon` from arguments.
@@ -36,10 +56,27 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
-
+function Car(model, milesPerGallon) {
+  this.model = model
+  this.milesPerGallon = milesPerGallon
+  this.tank = 0
+  this.odometer = 0
 }
-
+Car.prototype.fill = function(gallons){
+  this.tank = this.tank + gallons
+}
+Car.prototype.drive = function(distance){
+  const driveableMiles = this.tank * this.milesPerGallon
+  //these are driveable miles
+  if(distance <= driveableMiles){
+    this.odometer = this.odometer + distance;
+    this.tank = this.tank - (distance/this.milesPerGallon);
+  }else{
+    this.odometer = this.odometer + driveableMiles
+    this.tank = 0
+    return ` I ran out of fuel at ${this.odometer} miles`
+  }
+}
 
 /*
   TASK 3
@@ -48,19 +85,26 @@ function Car() {
     - Besides the methods on Person.prototype, babies also have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
+//baby is a baby of a Person
+function Baby(name, age, favoriteToy) {
+  //the baby is inherting these attributes
+  Person.call(this, name, age);
+  this.favoriteToy = favoriteToy
 
-function Baby() {
-
+}
+Baby.prototype = Object.create(Person.prototype);
+Baby.prototype.play = function(){
+  return `Playing with ${this.favoriteToy}`
 }
 
 
 /* 
   TASK 4
   In your own words explain the four principles for the "this" keyword below:
-  1. 
-  2. 
-  3. 
-  4. 
+  1. Window Binding - when none of the other rules apply, this will return the window or global object in the node or undefined in strict mode
+  2. Implicit binding - when the function is invoked look to the left of the dot that's what this refers to
+  3. Explicit binding - .call, .apply, .bind
+  4. New Binding - when a function is created as a constructor this points to a newly created object
 */
 
 ///////// END OF CHALLENGE /////////
